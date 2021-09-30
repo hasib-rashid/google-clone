216161f5948d7119448c18643fcac998f616b5ca
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-09-30 15:55:20 +0600

    📰Footer Now Done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-200 bottom-0 absolute">
-            <article>
+        <footer className="bg-gray-200 text-gray-600 bottom-0 absolute w-screen">
+            <article className="px-6 pl-10 py-3 border-b-2 border-gray-300">
                 Bangladesh
             </article>
-            <section>
-                <div className="">
-                    <li>About</li>
-                    <li>Advertising</li>
-                    <li>Business</li>
-                    <li>How Search Works</li>
+            <section className="flex justify-between px-6 py-3">
+                <div className="flex">
+                    <li className="mx-4">About</li>
+                    <li className="mx-4">Advertising</li>
+                    <li className="mx-4">Business</li>
+                    <li className="mx-4">How Search Works</li>
                 </div>
-                <div className="">
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                <div className="flex">
+                    <li className="mx-4">Privacy</li>
+                    <li className="mx-4">Term</li>
+                    <li className="mx-4">Settings</li>
                 </div>
             </section>
         </footer>
