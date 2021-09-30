92fbf7e782c710ad31d27c38371d44a34a908b43
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-09-30 10:08:09 +0600

    📰BoilerPlate

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { MicrophoneIcon, SearchIcon, ViewGridIcon } from '@heroicons/react/solid'
 
 const Home: NextPage = () => {
 	return (
-		<main className="w-screen h-screen bg-blue-600">
+		<main>
 			<h1>Hello World</h1>
 		</main>
 	)
