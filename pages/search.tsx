138bed9c60d229ee7cc80faea47f374a3974c952
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-10-01 09:53:26 +0600

    🔥Final Fixes

--- pages/search.tsx
+++ pages/search.tsx
@@ -24,16 +24,14 @@ export default function Search({ results }: any) {
 
 export async function getServerSideProps(context: any) {
     const data = await axios.request({
-        method: "GET", url: `https://google-search3.p.rapidapi.com/api/v1/search/q=${context.query.term}&num=1000`, headers: {
+        method: "GET", url: `https://google-search3.p.rapidapi.com/api/v1/search/q=${context.query.term}&num=1000&lr=lang_en&hl=en&cr=US`, headers: {
             'x-user-agent': 'desktop',
             'x-rapidapi-host': 'google-search3.p.rapidapi.com',
             'x-rapidapi-key': '616e3d86cbmsh19e94e8d6df2d94p13e47ejsne5e9296c68cb'
         }
     }).then(function (response) {
         return response.data
-    }).catch(function (error) {
-        console.error(error);
-    });
+    })
 
     return {
         props: {
