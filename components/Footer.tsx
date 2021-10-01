b71ac02f6fa6e93f7a121f237126a97a0cf7b7a2
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-10-01 12:51:01 +0600

    ♥Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="bg-gray-200 text-gray-600 bottom-0 absolute w-screen">
             <article className="px-6 pl-10 py-3 border-b-2 border-gray-300">
-                Bangladesh
+                Made with ❤ Hasib Al Rashid. No code copied from any other sources. <u>https://github.com/hasib-rashid/google-clone/</u>
             </article>
             <section className="flex justify-between px-6 py-3">
                 <div className="flex">
